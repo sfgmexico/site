b4d47f35ce725f6b77c2ce3ad677b3c6e084c5ba
elkevin <kaemspro@gmail.com>
2017-03-03 17:18:40 -0600

    LETTER

--- tcpdf/examples/pruebamulticell-marcos.php
+++ tcpdf/examples/pruebamulticell-marcos.php
@@ -28,7 +28,7 @@
 require_once('tcpdf_include.php');
 
 // create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, 'LETTER', true, 'UTF-8', false);
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
