cd01b6f53e66d4351f08ac61aef49d035525e248
zoomwas <zoomwas@hotmail.com>
2017-08-03 12:37:44 -0500

    cambios codigo postal

---
+++ cpostal.php
@@ -0,0 +1,40 @@
+<?php
+function leer($url,$cpostal){
+    try {
+            $xml = @simplexml_load_file($url);
+           
+    
+    $miArray =array();
+
+    foreach($xml->table as $item){
+        if($item->d_codigo==$cpostal){
+
+            $miArray[] = array("CodigoPostal"=>$item->d_codigo, "Municipio"=>$item->D_mnpio, "Estado"=>$item->d_estado, "Ciudad"=>$item->d_ciudad, "Fracc"=>$item->d_asenta);
+          
+
+           
+         
+        }
+      
+        
+    }
+
+     echo json_encode($miArray);
+           echo "<br><br><br>";
+    
+    foreach ($miArray as $key ) {
+               echo $key['Fracc']."<br>";
+           }
+    
+    }catch (Exception $e) {
+      echo 'Caught exception:---->>>> ',  $e->getMessage(), "\n";
+        exit();
+    }
+    
+    
+    
+  }
+
+
+  leer('xml/CPdescarga.xml',$_REQUEST['idCP']);
+  ?>
